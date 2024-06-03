084dfb890762260bcac15d58aa3c29e1780a0853
Adam Eladawy <adam.eladawy@gmail.com>
2024-06-03 11:43:32 +0100

    new and cool stuff

--- src/main/GamePanel.java
+++ src/main/GamePanel.java
@@ -84,9 +84,20 @@ public class GamePanel extends JPanel implements Runnable {
         tempScreen = new BufferedImage(screenWidth, screenHeight, BufferedImage.TYPE_INT_ARGB);
         g2 = (Graphics2D) tempScreen.getGraphics();
 
+        setFullScreen();
 
     }
 
+    public void setFullScreen(){
+        // GET LOCAL SCREEN DEVICE
+        GraphicsEnvironment ge = GraphicsEnvironment.getLocalGraphicsEnvironment();
+        GraphicsDevice gd = ge.getDefaultScreenDevice();
+        gd.setFullScreenWindow(Main.window);
+
+        // GET SCREEN HEIGHT AND WIDTH
+        screenWidth2 = Main.window.getWidth();
+        screenHeight2 = Main.window.getHeight();
+    }
     public void startGameThread() {
         gameThread = new Thread(this);
         gameThread.start();
